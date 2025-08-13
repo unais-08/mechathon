1fe30b5162a9a788b4530c4a1e99d99d99bf8476
Unais Shaikh <unais.shaikh.dev@gmail.com>
2025-08-13 21:22:19 +0530

    configure react with shadcn

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button";
+const App = () => {
+  return (
+    <>
+      <div>Hello From React!</div>
+      <Button>Hello</Button>
+    </>
+  );
+};
+
+export default App;
